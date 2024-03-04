a8a344aef95f7f654516fa19bf52deb41a20a229
shaheen <84270559+shaheen-senpai@users.noreply.github.com>
2024-03-04 21:28:02 +0530

    feat : mobile view

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,61 @@
 // app.js
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const { Sequelize, DataTypes } = require('sequelize');
+require('dotenv').config();
 
+const express = require('express');
+const { json } = require('body-parser');
+const { Sequelize, DataTypes } = require('sequelize');
+// const { auth } = require('express-openid-connect');
+// const { requiresAuth } = require('express-openid-connect');
+
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: process.env.SESSION_SECRET,
+//   baseURL: process.env.BASE_URL || 'http://localhost:8000/login',
+//   clientID: process.env.CLIENT_ID,
+//   issuerBaseURL: process.env.ISSUER_BASE_URL
+// };
+
+// using Auth0 api authorization 
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from YOUR_DOMAIN
+const jwtCheck = jwt.expressjwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: process.env.AUTH0_JWKS_URI
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: process.env.AUTH0_ISSUER,
+  algorithms: ["RS256"]
+});
+
+
+// Create an Express application
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+
+app.use(json());
+
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+// app.use(auth(config));
+
 
 // Replace 'your_database_name', 'your_username', and 'your_password' with your PostgreSQL database credentials
-const sequelize = new Sequelize('zaxqkdld', 'zaxqkdld', '7uw5QdHx_6s-Cjt-Gb5Ag-4idR5_Aq4g', {
-    host: 'trumpet.db.elephantsql.com',
-    dialect: 'postgres',
-    port: 5432, // PostgreSQL default port
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  
 });
 
 // Define the Todo model
@@ -67,10 +109,65 @@ const User = sequelize.define('User', {
 
 // Routes
 
+app.get('/', jwtCheck, (req, res) => {
+  if (!req.user) {
+    return res.status(401).send('User not authenticated');
+  }
+  else {
+    res.send('auth0 api authorization');
+  }
+});
+
+
+
+// // req.isAuthenticated is provided from the auth router and lets us know if the user is logged in
+// app.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
+
+// // custom login route to redirect to main page after login
+// app.get('/loginn', (req, res) =>
+//   res.oidc.login({
+//     returnTo: '/profile',
+//     authorizationParams: {
+//       redirect_uri: 'http://localhost:8000/index',
+//     },
+//   })
+// );
+
+// // custom logout route to redirect to login page after logout
+// app.get('/logoutt', (req, res) =>
+//   res.oidc.logout({
+//     returnTo: '/login',
+//   })
+// );
+
+// // return user profile and user id after login 
+// app.get('/profile', requiresAuth(), async (req, res) => {
+//   const username = req.oidc.user.name;
+
+//   try {
+//     const [user, created] = await User.findOrCreate({
+//       where: { username },
+//     });
+
+//     res.send({
+//       name: username,
+//       picture: req.oidc.user.picture,
+//       userId: user.id,
+//       created
+//     });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+
+
 // Get all todos based on user and type (myday, important, flagged)
-app.get('/todos/:userId/:type', async (req, res) => {
+app.get('/todos/:userId/:type', jwtCheck, async (req, res) => {
     const { userId, type } = req.params;
-  
+
     try {
       const user = await User.findByPk(userId, {
         include: [{
@@ -78,91 +175,90 @@ app.get('/todos/:userId/:type', async (req, res) => {
           where: { type },
         }],
       });
-  
+
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
       }
-  
+
       res.json(user.Todos);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   });
-  
+
   // Add a new todo for a specific user
-  app.post('/todos/:userId', async (req, res) => {
+  app.post('/todos/:userId', jwtCheck, async (req, res) => {
     const { userId } = req.params;
-  
+
     try {
       const user = await User.findByPk(userId);
-  
+
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
       }
-  
+
       const { title, description, type, status } = req.body;
       const todo = await Todo.create({ title, description, type, status });
       await user.addTodo(todo);
-  
+
       res.json(todo);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   });
-  
+
   // Delete a todo by ID for a specific user
-  app.delete('/todos/:userId/:id', async (req, res) => {
+  app.delete('/todos/:userId/:id', jwtCheck, async (req, res) => {
     const { userId, id } = req.params;
-  
+
     try {
       const user = await User.findByPk(userId);
-  
+
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
       }
-  
+
       const todo = await Todo.findByPk(id);
-  
+
       if (!todo) {
         return res.status(404).json({ error: 'Task not found' });
       }
-  
+
       await todo.destroy();
       res.json({ message: 'Task deleted successfully' });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   });
-  
+
   // Toggle the status of a todo between 'complete' and 'pending' by ID for a specific user
-  app.put('/todos/:userId/:id/toggle', async (req, res) => {
+  app.put('/todos/:userId/:id/toggle', jwtCheck, async (req, res) => {
     const { userId, id } = req.params;
-  
+
     try {
       const user = await User.findByPk(userId);
-  
+
       if (!user) {
         return res.status(404).json({ error: 'User not found' });
       }
-  
+
       const todo = await Todo.findByPk(id);
-  
+
       if (!todo) {
         return res.status(404).json({ error: 'Task not found' });
       }
-  
+
       // Toggle the status between 'complete' and 'pending'
       todo.status = todo.status === 'complete' ? 'pending' : 'complete';
       await todo.save();
-  
+
       res.json({ message: 'Task status toggled successfully' });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   });
-  
+
   // Start the server
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
-  
---
+++ backend/config.js
@@ -0,0 +1,8 @@
+  export const AUTH0_DOMAIN= 'dev-ci5h1y84jll0nhuf.us.auth0.com'
+  export const AUTH0_CLIENT= 'WD9jlgbGXU9iGGic8pLEyYGg2jsIfLqO'
+  export const AUTH0_AUDIENCE= 'https://trizwit-api'
+  export const BASE_FRONTEND_URL= 'http://localhost:8000'
+
+  
+
+  
